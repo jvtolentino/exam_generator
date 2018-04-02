84e0fcdcacab19cfe586e0d5673204feb3e96f3a
Jeffrey V. Tolentino <jvtolentino@gmail.com>
2018-04-02 16:25:13 +0800

    4:24P, initial template views using EJS

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-test again again
+const express = require('express');
+const path = require('path');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+app.listen(3000, () => {
+	console.log('Server started on Port 3000...');
+});
